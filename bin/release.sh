f7966dc2c5faca00b405b087f6d85d9968f5d38c
Chaoqian Xu <chaoranxu@gmail.com>
2019-12-16 15:42:06 +0800

    typo

--- bin/release.sh
+++ bin/release.sh
@@ -8,5 +8,5 @@ src_dir=${current_dir}/target/release
 target_dir=${current_dir}/releases/${release_name}
 link_file=${current_dir}/releases/current 
 mkdir -p ${target_dir}
-[ -d "${src_dir}" ] && cp -r ${src_dir}/ ${target_dir}/ \
+[ -d "${src_dir}" ] && cp -r ${src_dir}/* ${target_dir}/ \
  && rm -f ${link_file} && ln -fs ${target_dir} ${link_file}
